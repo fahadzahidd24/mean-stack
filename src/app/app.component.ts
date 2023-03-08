da36d8ed856c2c1447d37d7674d98eceeb0aa83d
Your Name <you@example.com>
2023-03-09 01:31:20 +0500

    app changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Mean_Stack_Udemyy';
+  title = 'Mean_Stack_Udemyyyy Fahad';
 }
